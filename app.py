aa6818408d963e0146c5aa22d70dd9de88fa8571
Sataphon Obra <85420217+puyepuye@users.noreply.github.com>
2023-11-05 10:55:59 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, send_from_directory
+
 from local_settings import SECRET_KEY
 import io, base64
 from PIL import Image
@@ -14,20 +15,20 @@ from PIL import Image
 import numpy as np
 
 import numpy as np
-# from keras.applications.vgg16 import VGG16, preprocess_input, decode_predictions
+from keras.applications.vgg16 import VGG16, preprocess_input, decode_predictions
 from PIL import Image
 import requests
 
 import webcolors
 from webcolors import rgb_to_name, CSS3_HEX_TO_NAMES
 import numpy as np
-from scipy.stats import mode
+# from scipy.stats import mode
 
 
 import os
 import openai
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 
 class Clothing:
@@ -38,26 +39,26 @@ class Clothing:
         self.type = type
         # self.size = size
 
-# def path_classi(image_path):
-#     # Step 1: Load and preprocess an image
-#     img = Image.open(image_path)
-#     img = img.resize((224, 224))  # Resize the image to match VGG16's input size
-#     x = np.array(img)
-#     x = np.expand_dims(x, axis=0)
-#     x = preprocess_input(x)
+def path_classi(image_path):
+    # Step 1: Load and preprocess an image
+    img = Image.open(image_path)
+    img = img.resize((224, 224))  # Resize the image to match VGG16's input size
+    x = np.array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
 
-#     # Step 2: Load the pre-trained VGG16 model
-#     model = VGG16(weights='imagenet')
+    # Step 2: Load the pre-trained VGG16 model
+    model = VGG16(weights='imagenet')
 
-#     # Step 3: Preprocess the image to match the format expected by VGG16
+    # Step 3: Preprocess the image to match the format expected by VGG16
 
-#     # Step 4: Make predictions using the pre-trained model
-#     predictions = model.predict(x)
+    # Step 4: Make predictions using the pre-trained model
+    predictions = model.predict(x)
 
-#     # Step 5: Decode and interpret the predictions
-#     decoded_predictions = decode_predictions(predictions, top=5)[0]
+    # Step 5: Decode and interpret the predictions
+    decoded_predictions = decode_predictions(predictions, top=5)[0]
 
-#     return decoded_predictions[0][1]
+    return decoded_predictions[0][1]
 
 app = Flask(__name__)
 
@@ -66,6 +67,10 @@ CAPTURED_IMAGES_DIR = "captured_images"
 
 # Create the directory if it doesn't exist
 os.makedirs(CAPTURED_IMAGES_DIR, exist_ok=True)
+
+@app.route("/")
+def hello():
+    return render_template("camera.html")
 
 @app.route('/camera')
 def camera():
@@ -92,27 +97,43 @@ def find_closest_color(rgb_color):
 
     return closest_color
 
-# def frame_to_color(path):
-#     img = Image.open(path)
-#     img = np.array(img)
-#     # Calculate the dimensions of the middle square
-#     middle_size = min(img.shape[0], img.shape[1]) // 2
+def calculate_mode(numbers):
+    # Create a dictionary to store the frequency of each number
+    frequency = {}
 
-#     # Calculate the coordinates of the top-left and bottom-right corners of the middle square
-#     top_left_x = (img.shape[1] - middle_size) // 2
-#     top_left_y = (img.shape[0] - middle_size) // 2
-#     bottom_right_x = top_left_x + middle_size
-#     bottom_right_y = top_left_y + middle_size
+    # Find the frequency of each number in the list
+    for number in numbers:
+        if number in frequency:
+            frequency[number] += 1
+        else:
+            frequency[number] = 1
 
-#     cropped_photo = img[top_left_y:bottom_right_y, top_left_x:bottom_right_x, :]
-#     reshaped_image = cropped_photo.reshape(-1, 3)
+    # Find the number(s) with the highest frequency
+    max_frequency = max(frequency.values())
+    mode = [number for number, freq in frequency.items() if freq == max_frequency]
 
-#     mode_color = mode(reshaped_image, axis=0)
+    return mode
+def frame_to_color(path):
+    img = Image.open(path)
+    img = np.array(img)
+    # Calculate the dimensions of the middle square
+    middle_size = min(img.shape[0], img.shape[1]) // 2
 
-#     mode_color = mode_color.mode[0]
+    # Calculate the coordinates of the top-left and bottom-right corners of the middle square
+    top_left_x = (img.shape[1] - middle_size) // 2
+    top_left_y = (img.shape[0] - middle_size) // 2
+    bottom_right_x = top_left_x + middle_size
+    bottom_right_y = top_left_y + middle_size
 
-#     single_color = np.full((100, 100, 3), mode_color, dtype=np.uint8)
-#     return find_closest_color(single_color)
+    cropped_photo = img[top_left_y:bottom_right_y, top_left_x:bottom_right_x, :]
+    reshaped_image = cropped_photo.reshape(-1, 3)
+
+    mode_color = calculate_mode(reshaped_image, axis=0)
+
+    mode_color = mode_color.mode[0]
+
+    single_color = np.full((100, 100, 3), mode_color, dtype=np.uint8)
+    return find_closest_color(single_color)
 
 
 
@@ -173,8 +194,9 @@ def classify_image():
             file.write(base64.b64decode(image_data.split(",")[1]))  # Extract base64 data
 
         # Run classification on the captured image
-        #image_description = path_classi(filename)
-        image_description = 'imagine it work'
+        image_description = path_classi(filename)
+        #image_description = 'sweater'
+        # image_color = frame_to_color(filename)
         image_color = 'blue'
         
         # Store the description in the dictionary along with the image path
@@ -191,9 +213,10 @@ def generate_question(clothes, temp, wind, precipitation):
     questions = "I have "
     for cloth in clothes:
         questions = questions + cloth.color + " " + cloth.type + ", "
+    
 
-    questions = questions + ". Today's tempurature is " + str(temp) + " degrees celsius, the wind is " + str(wind) + ", the precipitations is " + str(precipitation) + "percent. Please choose 5 clothing items for me to wear."
-
+    questions = questions + ". Today's tempurature is " + str(temp) + " degrees celsius, the wind is " + str(wind) + ", the precipitations is " + str(precipitation) + "percent. Please choose what to wear according to what I have"
+    print(questions)
     response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[{"role": "system", "content": questions}],
@@ -209,6 +232,8 @@ def generate_question(clothes, temp, wind, precipitation):
         if ": " in word:
             response_lst.append(word[:word.index(": ")])
 
+    if len(response_lst) == 0:
+        return "unable to give you suggestions, wear whatever you have"
     return response_lst
 
 
@@ -241,11 +266,15 @@ def qlouni():
 # print(testimg)
 
 
+@app.route('/captured_images/<filename>')
+def serve_image(filename):
+    return send_from_directory('captured_images', filename)
+
     
 if __name__ == '__main__':
-    #app.run()
+    app.run()
     #print(frame_to_color('archive/teetest.jpeg'))
-    print(generate_question([Clothing("Blue", "ack"), Clothing("Black", "rack"), Clothing("black", 'dress pants'), Clothing("grey", "hoodie"), Clothing("white", "hat"), Clothing("yellow", "raincoats")], 28, 3, 50))
+    #print(generate_question([Clothing("k","Blue", "ack"), Clothing("k","Black", "rack"), Clothing("k","black", 'dress pants'), Clothing("k","grey", "hoodie"), Clothing("k","white", "hat"), Clothing("k","yellow", "raincoats")], 50, 3, 50))
     #print(modify_str(generate_question([clothing("Blue", "T-Shirt"), clothing("Black", "Sneakers"), clothing("black", 'dress pants'), clothing("grey", "hoodie"), clothing("white", "hat"), clothing("yellow", "raincoats")], 28, 3, 50, "work")))
     #image_descriptions = {'captured_images/captured_image_5.jpg': ['wok', 'blue'], 'captured_images/captured_image_6.jpg': ['hand_blower', 'blue']}
     #print("me")
